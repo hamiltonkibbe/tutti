cb39684335411537c42fd382ca5af896dfe6e7f3
Tylor Dodge <tdodge@nexamp.com>
2025-02-24 13:59:04 -0500

    Add basic redis async support

--- tutti/backends/redis_asyncio.py
+++ tutti/backends/redis_asyncio.py
@@ -6,43 +6,44 @@
 """Redis distributed synchronization primitive backend"""
 
 import time
+from asyncio import sleep
 from typing import Optional
 
 import uuid
 
-from redis import Redis
-from redis.lock import Lock as RedisLock
+from redis.asyncio import Redis
+from redis.asyncio.lock import Lock as RedisLock
 
-from tutti.base import LockABC, SemaphoreABC
-from tutti.utils import get_connection_info, get_name_from_caller, RedisSemaphoreHandle
+from tutti.base import AsyncLockABC, AsyncSemaphoreABC
+from tutti.utils import get_connection_info, RedisSemaphoreHandle
 
 
-def acquire_lock(
+async def acquire_lock(
     conn: Redis,
-    lock_name: Optional[str] = None,
+    lock_name: str,
     blocking: bool = True,
     timeout: float = -1
-) -> Optional[RedisLock]:
-    lock_name = f"tutti-{lock_name if lock_name is not None else get_name_from_caller()}"
+) -> RedisLock | None:
+    lock_name = f"tutti-{lock_name}"
     lock = conn.lock(lock_name, timeout=None, blocking_timeout=None if not blocking else timeout)
     try:
-        lock.acquire()
+        await lock.acquire()
         return lock
     except:
         return None
 
 
-def release_lock(conn: Redis, lock: RedisLock) -> bool:
-    lock.release()
+async def __release_lock(conn: Redis, lock: RedisLock) -> bool:
+    await lock.release()
     return True
 
 
-def acquire_semaphore(
-        conn: Redis,
-        lock_name: str,
-        value: int = 1,
-        blocking: bool = True,
-        timeout: float = -1
+async def __acquire_semaphore(
+    conn: Redis,
+    lock_name: str,
+    value: int = 1,
+    blocking: bool = True,
+    timeout: float = -1
 ) -> Optional[RedisSemaphoreHandle]:
     identifier = str(uuid.uuid4())
     czset = f"{lock_name}-owner"
@@ -58,43 +59,48 @@ def acquire_semaphore(
 
         pipeline.zinterstore(czset, {czset: 1, lock_name: 0})
         pipeline.incr(ctr)
-        counter = pipeline.execute()[-1]
+        counter = await pipeline.execute()[-1]
         pipeline.zadd(lock_name, {identifier: now})
         pipeline.zadd(czset, {identifier: counter})
         pipeline.zrank(czset, identifier)
-        result = pipeline.execute()
+        result = await pipeline.execute()
         if result[-1] < value:
             return RedisSemaphoreHandle(lock_name, identifier)
         pipeline.zrem(lock_name, identifier)
         pipeline.zrem(czset, identifier)
-        pipeline.execute()
+        await pipeline.execute()
 
         if (not blocking) or (timeout >= 0 and time.time() > (now + timeout)):
             return None
-        time.sleep(0.001)
+        await sleep(0.001)
 
 
-def release_semaphore(conn: Redis, lock: RedisSemaphoreHandle) -> bool:
+async def __release_semaphore(conn: Redis, lock: RedisSemaphoreHandle) -> bool:
     pipeline = conn.pipeline(transaction=True)
     pipeline.zrem(lock.name, lock.identifier)
     pipeline.zrem(f"{lock.name}-owner", lock.identifier)
-    result = pipeline.execute()
+    result = await pipeline.execute()
     return bool(result[0])
 
 
-class Lock(LockABC):
+class Lock(AsyncLockABC):
 
-    def __init__(self, blocking: bool = True, timeout: Optional[float] = None, lock_name: Optional[str] = None) -> None:
+    def __init__(
+        self,
+        lock_name: str,
+        blocking: bool = True,
+        timeout: Optional[float] = None,
+    ) -> None:
         self._conn = Redis(**get_connection_info())
         self._handle: Optional[RedisLock] = None
         self._blocking = blocking
         self._timeout = timeout
-        self._lock_name = lock_name if lock_name is not None else get_name_from_caller()
+        self._lock_name = lock_name
 
-    def acquire(self, blocking: bool = True, timeout: Optional[float] = None) -> bool:
+    async def acquire(self, blocking: bool = True, timeout: Optional[float] = None) -> bool:
         lock = self._conn.lock(self._lock_name, timeout=None, blocking_timeout=timeout)
         try:
-            result = lock.acquire()
+            result = await lock.acquire()
             if result:
                 self._handle = lock
             return result
@@ -102,56 +108,67 @@ class Lock(LockABC):
             self._handle = None
             return False
 
-    def release(self) -> None:
+    async def release(self) -> None:
         if self._handle is None:
             raise RuntimeError("Attempt to release unlocked lock.")
-        release_lock(self._conn, self._handle)
+        await __release_lock(self._conn, self._handle)
 
-    def locked(self) -> bool:
+    async def locked(self) -> bool:
         lock = self._conn.lock(self._lock_name)
-        return lock.locked()
+        return await lock.locked()
 
-    def __enter__(self) -> "Lock":
-        acquired = self.acquire(self._blocking, self._timeout)
+    async def __aenter__(self) -> "Lock":
+        acquired = await self.acquire(self._blocking, self._timeout)
         if not acquired:
             raise RuntimeError("Unable to acquire lock")
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        return self.release()
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        return await self.release()
 
 
-class Semaphore(SemaphoreABC):
-    def __init__(self, value: int = 1, lock_name: Optional[str] = None):
+class Semaphore(AsyncSemaphoreABC):
+    def __init__(self, lock_name: str, value: int = 1):
         self._conn = Redis(**get_connection_info())
         self._value = value
         self._handle: Optional[RedisSemaphoreHandle] = None
-        self._lock_name = lock_name if lock_name is not None else get_name_from_caller()
+        self._lock_name = lock_name
 
-    def acquire(self, blocking: bool = True, timeout: Optional[float] = None) -> bool:
+    async def acquire(
+        self,
+        blocking: bool = True,
+        timeout: Optional[float] = None
+    ) -> bool:
         timeout_float = -1 if timeout is None else timeout
-        with Lock(blocking, timeout, lock_name=f"{self._lock_name}-lock"):
-            self._handle = acquire_semaphore(self._conn, value=self._value, lock_name=self._lock_name, blocking=blocking, timeout=timeout_float)
+        lock_name = f"{self._lock_name}-lock"
+        async with Lock(lock_name, blocking, timeout):
+            self._handle = await __acquire_semaphore(
+                self._conn,
+                value=self._value,
+                lock_name=self._lock_name,
+                blocking=blocking,
+                timeout=timeout_float
+            )
             return self._handle is not None
 
-    def release(self, n: int = 1) -> None:
+    async def release(self, n: int = 1) -> None:
         if self._handle is not None:
-            release_semaphore(self._conn, self._handle)
+            await __release_semaphore(self._conn, self._handle)
 
-    def __enter__(self) -> "Semaphore":
-        acquired = self.acquire()
+    async def __aenter__(self) -> "Semaphore":
+        acquired = await self.acquire()
         if not acquired:
             raise RuntimeError("Unable to acquire semaphore")
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         if self._handle:
-            return self.release()
+            return await self.release()
 
 
 class BoundedSemaphore(Semaphore):
-    def release(self, n: int = 1) -> None:
-        if self._handle is None or not release_semaphore(self._conn, self._handle):
+    async def release(self, n: int = 1) -> None:
+        if self._handle is None or not  await __release_semaphore(self._conn, self._handle):
             raise ValueError("Semaphore released too many times")
 
 
--- tutti/utils.py
+++ tutti/utils.py
@@ -8,6 +8,9 @@ import inspect
 import os
 import sys
 
+from typing import NamedTuple, TypedDict
+
+
 def get_name_from_caller():
     previous_frame = inspect.currentframe().f_back.f_back
     filename, line_no, function_name, lines, index = inspect.getframeinfo(previous_frame)
@@ -15,3 +18,22 @@ def get_name_from_caller():
     hash_filename = filename.split("/")[-1] if relfilename == "." else relfilename
     to_hash = ",".join([hash_filename, str(line_no), function_name])
     return hashlib.sha256(to_hash.encode()).hexdigest()
+
+
+class RedisSemaphoreHandle(NamedTuple):
+    name: str
+    identifier: str
+
+
+class RedisConnectionInfo(TypedDict):
+    host: str
+    port: int
+    db: int
+
+
+def get_connection_info() -> RedisConnectionInfo:
+    return {
+        "host": os.getenv("TUTTI_REDIS_HOST", "localhost"),
+        "port": int(os.getenv("TUTTI_REDIS_PORT", 6379)),
+        "db": int(os.getenv("TUTTI_REDIS_DB", 0)),
+    }
