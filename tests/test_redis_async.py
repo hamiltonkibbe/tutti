3cf95637778147ab5d1e8224e713ae592d09e5de
Tylor Dodge <tdodge@nexamp.com>
2025-02-27 20:35:47 -0500

    Add tests for redis async lock and semaphore

---
+++ tests/test_redis_async.py
@@ -0,0 +1,236 @@
+from unittest.mock import AsyncMock
+
+import pytest
+
+from redis.asyncio import Redis
+from redis.asyncio.lock import Lock as RedisLock
+
+from tutti.backends.redis.asyncio import Lock, Semaphore, RedisWrapper
+from tutti.backends.redis.types import RedisSemaphoreHandle
+
+
+# region mocks
+
+class MockRedisLock(RedisLock):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class MockRedisConnection(Redis):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+# endregion
+
+async def test_lock_success():
+    """
+    Given:
+        - A RedisWraper mock that executes correctly
+        - A Tutti redis Lock
+
+    When:
+        - The lock is acquired
+        - The lock is released
+
+    Then:
+        - The lock is acquired and released successfully
+    """
+    # Given
+    mock_redis_lock = MockRedisLock()
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.return_value = (mock_redis_lock, True)
+    mock_redis_wrapper.release_lock.return_value = True
+
+    # When
+    async with Lock(
+        lock_name="test_lock",
+        timeout=10,
+        conn=MockRedisConnection(),
+        redis_wrapper=mock_redis_wrapper
+    ) as lock:
+        # Then
+        assert lock._handle == mock_redis_lock
+
+    mock_redis_wrapper.acquire_lock.assert_awaited_once()
+    mock_redis_wrapper.release_lock.assert_awaited_once()
+
+
+async def test_lock_failure_to_acquire():
+    """
+    Given:
+        - A RedisWraper mock that does not azquire the lock
+        - A Tutti redis Lock
+    
+    When:
+        - The lock is acquired
+        - The lock is released
+    
+    Then:
+        - A RuntimeError error is raised
+        - acquire_lock is awaited
+        - release_lock is not awaited
+    """
+    # Given
+    mock_redis_lock = MockRedisLock()
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.return_value = (mock_redis_lock, False)
+
+    # When
+    with pytest.raises(RuntimeError):
+        async with Lock(
+            lock_name="test_lock",
+            timeout=10,
+            conn=MockRedisConnection(),
+            redis_wrapper=mock_redis_wrapper
+        ) as lock:
+            # Then
+            assert lock._handle is None
+
+    mock_redis_wrapper.acquire_lock.assert_awaited_once()
+    mock_redis_wrapper.release_lock.assert_not_awaited()
+
+
+async def test_lock_raises():
+    """
+    Given:
+        - A RedisWraper mock that raises when acquiring the lock
+        - A Tutti redis Lock
+    
+    When:
+        - The lock is acquired
+        - The lock is released
+    
+    Then:
+        - A RuntimeError error is raised
+        - acquire_lock is awaited
+        - release_lock is not awaited
+    """
+    # Given
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.side_effect = ValueError("something happened")
+
+    # When
+    with pytest.raises(RuntimeError):
+        async with Lock(
+            lock_name="test_lock",
+            timeout=10,
+            conn=MockRedisConnection(),
+            redis_wrapper=mock_redis_wrapper
+        ) as lock:
+            # Then
+            assert lock._handle is None
+
+    mock_redis_wrapper.acquire_lock.assert_awaited_once()
+    mock_redis_wrapper.release_lock.assert_not_awaited()
+
+
+async def test_semaphore_success():
+    """
+    Given:
+        - A RedisWraper mock that executes correctly
+        - A Tutti redis Semaphore
+
+    When:
+        - The semaphore is acquired
+        - The semaphore is released
+
+    Then:
+        - The semaphore is acquired and released successfully
+    """
+    # Given
+    semaphore_handle = RedisSemaphoreHandle("test_lock", "whatever")
+    mock_redis_lock = MockRedisLock()
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.return_value = (mock_redis_lock, True)
+    mock_redis_wrapper.release_lock.return_value = True
+    mock_redis_wrapper.acquire_semaphore.return_value = semaphore_handle
+    mock_redis_wrapper.release_semaphore.return_value = True
+
+    # When
+    async with Semaphore(
+        lock_name="test_lock",
+        value=1,
+        timeout=10,
+        conn=MockRedisConnection(),
+        redis_wrapper=mock_redis_wrapper
+    ) as semaphore:
+        # Then
+        assert semaphore._handle == semaphore_handle
+
+    mock_redis_wrapper.acquire_semaphore.assert_awaited_once()
+    mock_redis_wrapper.release_semaphore.assert_awaited_once()
+
+
+async def test_semaphore_fail_to_acquire():
+    """
+    Given:
+        - A RedisWraper mock that fails to acquire the semaphore
+        - A Tutti redis Semaphore
+
+    When:
+        - The semaphore is acquired
+        - The semaphore is released
+
+    Then:
+        - A RuntimeError error is raised
+        - acquire_semaphore is awaited
+        - release_semaphore is not awaited
+    """
+    # Given
+    mock_redis_lock = MockRedisLock()
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.return_value = (mock_redis_lock, True)
+    mock_redis_wrapper.release_lock.return_value = True
+    mock_redis_wrapper.acquire_semaphore.return_value = None
+
+    # When
+    with pytest.raises(RuntimeError):
+        async with Semaphore(
+            lock_name="test_lock",
+            value=1,
+            timeout=10,
+            conn=MockRedisConnection(),
+            redis_wrapper=mock_redis_wrapper
+        ) as semaphore:
+            # Then
+            assert semaphore._handle is None
+
+    mock_redis_wrapper.acquire_semaphore.assert_awaited_once()
+    mock_redis_wrapper.release_semaphore.not_awaited()
+
+
+async def test_semaphore_fail_to_acquire_lock():
+    """
+    Given:
+        - A RedisWraper mock that raises when acquiring the lock
+        - A Tutti redis Semaphore
+
+    When:
+        - The semaphore is acquired
+        - The semaphore is released
+
+    Then:
+        - A RuntimeError error is raised
+        - acquire_semaphore is not awaited
+        - release_semaphore is not awaited
+    """
+    # Given
+    mock_redis_wrapper = AsyncMock(spec=RedisWrapper)
+    mock_redis_wrapper.acquire_lock.side_effect = ValueError("something happened")
+
+    # When
+    with pytest.raises(RuntimeError):
+        async with Semaphore(
+            lock_name="test_lock",
+            value=1,
+            timeout=10,
+            conn=MockRedisConnection(),
+            redis_wrapper=mock_redis_wrapper
+        ) as semaphore:
+            # Then
+            assert semaphore._handle is None
+
+    mock_redis_wrapper.acquire_semaphore.not_awaited()
+    mock_redis_wrapper.release_semaphore.not_awaited()
+
