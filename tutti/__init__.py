b6f44d9949df44436e96e88a70eeae983f180af2
Tylor Dodge <tdodge@nexamp.com>
2025-03-04 08:57:33 -0500

    Update public API, add factories, and redis singleton

--- tutti/__init__.py
+++ tutti/__init__.py
@@ -3,19 +3,10 @@
 #
 # Copyright 2021 Hamilton Kibbe <ham@hamiltonkib.be
 
-import os
-backend = os.getenv("TUTTI_BACKEND", "redis")
-if backend == "redis":
-    from .backends.redis import Lock
-    from .backends.redis import Semaphore
-    from .backends.redis import BoundedSemaphore
-elif backend == "redis-async":
-    from .backends.redis import AsyncLock as Lock
-    from .backends.redis import AsyncSemaphore as Semaphore
-    from .backends.redis import AsyncBoundedSemaphore as BoundedSemaphore
-else:
-    raise RuntimeError("Unrecognized backend: {backend}")
+from .sync import Lock, Semaphore, BoundedSemaphore
+from .backends.redis.types import RedisLockConfig
+from .backends.redis.types import RedisSemaphoreConfig
 
 
 __version__ = '1.0.0'
-__all__ = ["Lock", "Semaphore", "BoundedSemaphore"]
+__all__ = ["Lock", "Semaphore", "BoundedSemaphore", "RedisLockConfig", "RedisSemaphoreConfig"]
--- tutti/backends/redis/types.py
+++ tutti/backends/redis/types.py
@@ -3,9 +3,27 @@
 #
 # Copyright 2021 Hamilton Kibbe <ham@hamiltonkib.be>
 
+from dataclasses import dataclass
 from typing import NamedTuple
+
+from tutti.base import LockConfig, SemaphoreConfig
 
 
 class RedisSemaphoreHandle(NamedTuple):
     name: str
     identifier: str
+
+
+@dataclass
+class RedisLockConfig(LockConfig):
+    connection_url: str
+    name: str
+    timeout: float
+    blocking: bool = True
+
+
+@dataclass
+class RedisSemaphoreConfig(SemaphoreConfig):
+    connection_url: str
+    max_concurrency: int
+    lock: RedisLockConfig
